c91800bd52613a3951624fb8d5ce14447e26dbbd
Nater Kane <nater@naterkane.com>
2014-07-09 09:59:00 -0400

    renamed some files, and tweaked the Behavior so the php strict complaints would stop.

--- Model/Behavior/FileUploadBehavior.php
+++ Model/Behavior/FileUploadBehavior.php
@@ -21,12 +21,12 @@
   *
   *
   * @note: Please review the plugins/file_upload/config/file_upload_settings.php file for details on each setting.
-  * @version: since 6.1.0
-  * @author: Nick Baker
-  * @link: http://www.webtechnick.com
+  * @version: since 0.1
+  * @author: Nater Kane
+  * @link: http://github.com/naterkane
   */
 App::import('Vendor', 'FileUpload.uploader');
-require_once(dirname(__FILE__) . DS . '..' . DS . '..' . DS . 'config' . DS . 'file_upload_settings.php');
+require_once(dirname(__FILE__) . DS . '..' . DS . '..' . DS . 'Config' . DS . 'FileUploadSettings.php');
 class FileUploadBehavior extends ModelBehavior {
   
   /**
@@ -34,36 +34,53 @@ class FileUploadBehavior extends ModelBehavior {
     */
   var $Uploader = array();
   
+	/**
+	 * Behavior settings
+	 *
+	 * @var array
+	 */
+		public $settings = array();
+
+	/**
+	 * Default setting values
+	 *
+	 * @var array
+	 */
+		protected $_defaults = array();
+
+	
   function setFileUploadOption(&$Model, $key, $value) {
     $this->options[$Model->alias][$key] = $value;
     $this->Uploader[$Model->alias]->setOption($key, $value);
   }
   
-  /**
-    * Setup the behavior
-    */
-  function setUp(&$Model, $options = array()){
+	/**
+	 * Setup the behavior and import required classes.
+	 *
+	 * @param \Model|object $Model Model using the behavior
+	 * @param array $settings Settings to override for model.
+	 * @return void
+	 */
+  public function setup(Model $Model, $config = Array()){
 	  
     $FileUploadSettings = new FileUploadSettings();
-    if(!is_array($options)){
-      $options = array();
+    if(is_array($config)){
+      $this->config[$Model->alias] = array_merge($FileUploadSettings->defaults, $config);
+    } else {
+    	$this->config[$Model->alias] = $this->_defaults;
     }
-    $this->options[$Model->alias] = array_merge($FileUploadSettings->defaults, $options);
-        
-    $uploader_settings = $this->options[$Model->alias];
-    $uploader_settings['uploadDir'] = $this->options[$Model->alias]['forceWebroot'] ? WWW_ROOT . $uploader_settings['uploadDir'] : $uploader_settings['uploadDir']; 
+    
+    $uploader_settings = $this->config[$Model->alias];
+    $uploader_settings['uploadDir'] = $this->config[$Model->alias]['forceWebroot'] ? WWW_ROOT . $uploader_settings['uploadDir'] : $uploader_settings['uploadDir']; 
     $uploader_settings['fileModel'] = $Model->alias;
     $this->Uploader[$Model->alias] = new Uploader($uploader_settings);
-	//var_dump($this->Uploader);
-	//echo "upload directory " . $uploader_settings['uploadDir'];
   }
  
   /**
     * beforeSave if a file is found, upload it, and then save the filename according to the settings
     *
     */
-  function beforeSave(&$Model){
-	  ?>beforesave has been called.<?php
+  public function beforeSave(Model $Model, $options = Array()){
     if(isset($Model->data[$Model->alias][$this->options[$Model->alias]['fileVar']])){
       $file = $Model->data[$Model->alias][$this->options[$Model->alias]['fileVar']];
       $this->Uploader[$Model->alias]->file = $file;
@@ -90,8 +107,7 @@ class FileUploadBehavior extends ModelBehavior {
     * Updates validation errors if there was an error uploading the file.
     * presents the user the errors.
     */
-  function beforeValidate(&$Model){
-	  ?>beforevalidate has been called.<?php
+  public function beforeValidate(Model $Model, $options = Array()){
     if(isset($Model->data[$Model->alias][$this->options[$Model->alias]['fileVar']])){
       $file = $Model->data[$Model->alias][$this->options[$Model->alias]['fileVar']];
       $this->Uploader[$Model->alias]->file = $file;
@@ -118,11 +134,11 @@ class FileUploadBehavior extends ModelBehavior {
   /**
     * Automatically remove the uploaded file.
     */
-  function beforeDelete(&$Model, $cascade){
+  public function beforeDelete(Model $Model, $cascade = true){
     $Model->recursive = -1;
     $data = $Model->read();
     
-    $this->Uploader[$Model->alias]->removeFile($data[$Model->alias][$this->options[$Model->alias]['fields']['name']]);
+   $this->Uploader[$Model->alias]->removeFile($data[$Model->alias][$this->options[$Model->alias]['fields']['name']]);
     return $Model->beforeDelete($cascade);
   }
   
